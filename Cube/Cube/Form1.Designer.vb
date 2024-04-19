26e1107d3279c058b050d807d3063f63ce46deb8
Kyosuke Miyazawa <31465059+zenmai36rai@users.noreply.github.com>
2024-04-19 17:16:31 +0900

    Cube

---
+++ Cube/Cube/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(21, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(361, 313)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Cube/Cube/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        PictureBox1.BackColor = Color.White
+        Dim canvas As New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Dim g As Graphics = Graphics.FromImage(canvas)
+        g.DrawLine(Pens.Black, 50, 50, 250, 50)
+        g.DrawLine(Pens.Black, 250, 50, 270, 20)
+        g.DrawLine(Pens.Black, 270, 20, 70, 20)
+        g.DrawLine(Pens.Black, 70, 20, 50, 50)
+        g.DrawLine(Pens.Black, 50, 50, 50, 250)
+        g.DrawLine(Pens.Black, 250, 50, 250, 250)
+        g.DrawLine(Pens.Black, 270, 20, 270, 220)
+        g.DrawLine(Pens.Black, 70, 20, 70, 220)
+        g.DrawLine(Pens.Black, 50, 250, 250, 250)
+        g.DrawLine(Pens.Black, 250, 250, 270, 220)
+        g.DrawLine(Pens.Black, 270, 220, 70, 220)
+        g.DrawLine(Pens.Black, 70, 220, 50, 250)
+        PictureBox1.Image = canvas
+    End Sub
+End Class
